cdb814d39779a3373a99869e30c7cf195e5c91cf
Arit <arit@example.com>
2026-01-06 15:51:45 +0530

    Fix metro config for Expo SDK 50: Use @expo/metro-config instead of @react-native/metro-config

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,11 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://docs.expo.dev/guides/customizing-metro
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('expo/metro-config').MetroConfig}
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = getDefaultConfig(__dirname, config);
